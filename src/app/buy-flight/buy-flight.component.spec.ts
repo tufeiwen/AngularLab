0d7bde58a080ef37c65cfdfce944bddc18f89380
DESKTOP-9L7HK0B\User <wenxngood@yahoo.com>
2017-04-10 19:52:17 -0400

    lab 3.1 init (Solutions\Ex2.2_Bonus_2)

---
+++ src/app/buy-flight/buy-flight.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BuyFlightComponent } from './buy-flight.component';
+
+describe('Component: BuyFlight', () => {
+  it('should create an instance', () => {
+    let component = new BuyFlightComponent();
+    expect(component).toBeTruthy();
+  });
+});
